96889d88060e456dfee0ecc6ef1ebe0610ae8e47
AbrilOrtega2 <aortega2@mail.austral.edu.ar>
2025-08-11 13:05:50 -0300

    Add files via upload

--- exercise_math.py
+++ exercise_math.py
@@ -1,3 +1,17 @@
 def math():
     a = 57
     b = 7
+    print (a+b) #suma
+    resta= a-b
+    print (resta)
+    #producto
+    print (a*b)
+    #promedio 
+    print ((a+b)/2)
+    #cociente entero
+    print (a//b)
+    #resto de la division entera
+    print (a%b)
+    #valor real de la division 
+    print(a/b)
+math()
